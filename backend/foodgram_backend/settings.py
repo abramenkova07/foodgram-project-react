7308c63a9a5d7c8ccdaffe086bdc5d5176e27deb
Арина Абраменкова <filippova-arina17@yandex.ru>
2024-04-11 18:42:31 +0300

    CSRF

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -14,6 +14,8 @@ DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
+CSRF_TRUSTED_ORIGINS = ['https://foodgram.run.place']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
