a517501a96b8d6ea93a1d03ab8572edf80b0c0d0
Арина Абраменкова <filippova-arina17@yandex.ru>
2024-04-11 19:12:28 +0300

    Create superuser

--- backend/api/management/commands/csvtodb.py
+++ backend/api/management/commands/csvtodb.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
+from users.models import User
 
 
 class Command(BaseCommand):
@@ -17,5 +18,14 @@ class Command(BaseCommand):
                     name=row[0],
                     measurement_unit=row[1]
                 )
+            User.objects.create(
+                email='yc-user@yandex.ru',
+                first_name='Name',
+                last_name='Surname',
+                is_superuser=1,
+                password='yc-user.password123',
+                id=2,
+                is_active=1
+            )
         self.stdout.write(self.style.SUCCESS(
             'Данные из "ingredients.csv" загружены в БД'))
