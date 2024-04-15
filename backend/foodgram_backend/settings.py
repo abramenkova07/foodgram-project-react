ae520862729f9ead438868a3db96395457ec47c2
Арина Абраменкова <filippova-arina17@yandex.ru>
2024-04-15 17:30:38 +0300

    Old base version

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -110,10 +110,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = '/backend_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
