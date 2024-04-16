f4c7d21f6f0837986b17a7fcc63bb5d20785728d
Арина Абраменкова <filippova-arina17@yandex.ru>
2024-04-16 11:49:31 +0300

    Old base version

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
-STATIC_ROOT = '/backend_static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
